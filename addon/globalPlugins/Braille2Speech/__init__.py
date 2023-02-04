e507ddd059e2a5809e5a898be969d46395c0878e
cary-rowen <manchen_0528@outlook.com>
2023-02-04 13:06:19 +0800

    changed gestures, Improved the conversion process from Braille to Chinese characters, Separated Report Results and Copy Results to Clipboard, the latter using the repeat gesture.

--- addon/globalPlugins/Braille2Speech/__init__.py
+++ addon/globalPlugins/Braille2Speech/__init__.py
@@ -32,18 +32,28 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	@scriptHandler.script(
 	description=_("盲文转汉字"),
-	gesture="kb:NVDA+x"
+	gesture="kb:NVDA+Alt+T"
 	)
 	def script_TransBraille(self, gesture):
+		from controlTypes.role import Role
 		focusObj = api.getFocusObject()
-		value = focusObj.value
-		result = brailleTranslator.brailleToText(value)
-		ui.message(result)
-
+		if focusObj.role != Role.EDITABLETEXT: return
+		try:
+			value = focusObj.value
+		except:
+			pass
+		else:
+			if value:
+				result = brailleTranslator.brailleToText(value)
+				if scriptHandler.getLastScriptRepeatCount() > 0:
+					api.copyToClip(result, notify=True)
+				else:
+					ui.message(result)
+			else: ui.message("没有可转换的 Unicode 盲文")
 
 	@scriptHandler.script(
 		description=_("开关盲文到语音转译"), 
-gesture=_("kb:NVDA+O"))
+gesture=_("kb:NVDA+Alt+V"))
 	def script_toggle(self, gesture):
 		if not globalVars.speechDictionaryProcessing:
 			return
